b45e6b7804c91c1f8ff6ac41b54ea6f3b63c1ed5
James Hall <james.hall01@outlook.com>
2026-05-02 00:21:52 +0100

    chore(neoforge): drop deprecated @OnlyIn from empty initClient stub

--- forge/src/main/java/dev/duzo/players/PlayersForge.java
+++ forge/src/main/java/dev/duzo/players/PlayersForge.java
@@ -1,11 +1,8 @@
 package dev.duzo.players;
 
 import dev.duzo.players.platform.ForgeCommonRegistry;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.loading.FMLLoader;
 
 @Mod(Constants.MOD_ID)
 public class PlayersForge {
@@ -13,13 +10,5 @@ public class PlayersForge {
         PlayersCommon.init();
 
         ForgeCommonRegistry.init(modEventBus);
-
-        if (FMLLoader.getDist() == Dist.CLIENT) {
-            initClient(modEventBus);
-        }
-    }
-
-    @OnlyIn(Dist.CLIENT)
-    private static void initClient(IEventBus modEventBus) {
     }
 }
