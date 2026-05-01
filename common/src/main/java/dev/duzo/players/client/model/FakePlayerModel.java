b5427a0bb31b28986373f4a7816ae34764eca549
James Hall <james.hall01@outlook.com>
2026-05-01 23:31:41 +0100

    fix: dont copy arm rotation to sleeves, theyre children now

--- common/src/main/java/dev/duzo/players/client/model/FakePlayerModel.java
+++ common/src/main/java/dev/duzo/players/client/model/FakePlayerModel.java
@@ -16,29 +16,16 @@ public class FakePlayerModel extends PlayerModel {
 		super.setupAnim(state);
 
 		if (state instanceof FakePlayerRenderState fake && fake.isSitting && !state.isPassenger) {
-			this.translateSitting();
+			this.rightArm.xRot += (-(float) Math.PI / 5F);
+			this.leftArm.xRot += (-(float) Math.PI / 5F);
+
+			this.rightLeg.xRot = -1.4137167F;
+			this.rightLeg.yRot = ((float) Math.PI / 10F);
+			this.rightLeg.zRot = 0.07853982F;
+
+			this.leftLeg.xRot = -1.4137167F;
+			this.leftLeg.yRot = (-(float) Math.PI / 10F);
+			this.leftLeg.zRot = -0.07853982F;
 		}
-	}
-
-	private void translateSitting() {
-		this.rightArm.xRot += (-(float) Math.PI / 5F);
-		this.rightSleeve.xRot += (-(float) Math.PI / 5F);
-
-		this.leftArm.xRot += (-(float) Math.PI / 5F);
-		this.leftSleeve.xRot += (-(float) Math.PI / 5F);
-
-		this.rightLeg.xRot = -1.4137167F;
-		this.rightLeg.yRot = ((float) Math.PI / 10F);
-		this.rightLeg.zRot = 0.07853982F;
-		this.rightPants.xRot = -1.4137167F;
-		this.rightPants.yRot = ((float) Math.PI / 10F);
-		this.rightPants.zRot = 0.07853982F;
-
-		this.leftLeg.xRot = -1.4137167F;
-		this.leftLeg.yRot = (-(float) Math.PI / 10F);
-		this.leftLeg.zRot = -0.07853982F;
-		this.leftPants.xRot = -1.4137167F;
-		this.leftPants.yRot = (-(float) Math.PI / 10F);
-		this.leftPants.zRot = -0.07853982F;
 	}
 }
