88e1d6151010b2bc9e0e4eec72232b7a354f92ab
James Hall <james.hall01@outlook.com>
2026-05-08 17:52:54 +0100

    feat(ai): ai sub-menu with bond, jobs, waypoint/region/deposit markers (#27)

--- common/src/main/java/dev/duzo/players/network/PlayersNetwork.java
+++ common/src/main/java/dev/duzo/players/network/PlayersNetwork.java
@@ -2,10 +2,14 @@ package dev.duzo.players.network;
 
 import commonnetwork.api.Network;
 import dev.duzo.players.network.c2s.ApplyFakePlayerSkinPacketC2S;
+import dev.duzo.players.network.c2s.BondPacketC2S;
 import dev.duzo.players.network.c2s.CyclePosePacketC2S;
+import dev.duzo.players.network.c2s.GiveAIMarkerPacketC2S;
 import dev.duzo.players.network.c2s.RequestSkinDataPacketC2S;
 import dev.duzo.players.network.c2s.SetFakePlayerNamePacketC2S;
+import dev.duzo.players.network.c2s.SetJobPacketC2S;
 import dev.duzo.players.network.c2s.SetSkinKeyPacketC2S;
+import dev.duzo.players.network.c2s.StartStopJobPacketC2S;
 import dev.duzo.players.network.c2s.ToggleFakePlayerFlagPacketC2S;
 import dev.duzo.players.network.c2s.UploadSkinPacketC2S;
 import dev.duzo.players.network.s2c.OpenScreenPacketS2C;
@@ -22,5 +26,9 @@ public class PlayersNetwork {
 		Network.registerPacket(UploadSkinPacketC2S.LOCATION, UploadSkinPacketC2S.class, UploadSkinPacketC2S::encode, UploadSkinPacketC2S::decode, UploadSkinPacketC2S::handle);
 		Network.registerPacket(RequestSkinDataPacketC2S.LOCATION, RequestSkinDataPacketC2S.class, RequestSkinDataPacketC2S::encode, RequestSkinDataPacketC2S::decode, RequestSkinDataPacketC2S::handle);
 		Network.registerPacket(SkinDataPacketS2C.LOCATION, SkinDataPacketS2C.class, SkinDataPacketS2C::encode, SkinDataPacketS2C::decode, SkinDataPacketS2C::handle);
+		Network.registerPacket(BondPacketC2S.LOCATION, BondPacketC2S.class, BondPacketC2S::encode, BondPacketC2S::decode, BondPacketC2S::handle);
+		Network.registerPacket(SetJobPacketC2S.LOCATION, SetJobPacketC2S.class, SetJobPacketC2S::encode, SetJobPacketC2S::decode, SetJobPacketC2S::handle);
+		Network.registerPacket(StartStopJobPacketC2S.LOCATION, StartStopJobPacketC2S.class, StartStopJobPacketC2S::encode, StartStopJobPacketC2S::decode, StartStopJobPacketC2S::handle);
+		Network.registerPacket(GiveAIMarkerPacketC2S.LOCATION, GiveAIMarkerPacketC2S.class, GiveAIMarkerPacketC2S::encode, GiveAIMarkerPacketC2S::decode, GiveAIMarkerPacketC2S::handle);
 	}
 }
