82289962ca1bd4189d76b312a3151d7a53167c00
James Hall <james.hall01@outlook.com>
2026-06-27 15:41:04 +0100

    feat(jobs): add crafter job with learn-by-example recipe gui

--- fabric/src/main/java/dev/duzo/players/client/PlayersFabricClient.java
+++ fabric/src/main/java/dev/duzo/players/client/PlayersFabricClient.java
@@ -5,6 +5,7 @@ import dev.duzo.players.client.renderers.FakeFishingHookRenderer;
 import dev.duzo.players.client.renderers.FakePlayerRendererWrapper;
 import dev.duzo.players.client.renderers.FishingLineRenderer;
 import dev.duzo.players.client.renderers.LegacyRodCast;
+import dev.duzo.players.client.screen.FakeCrafterScreen;
 import dev.duzo.players.client.screen.FakePlayerInventoryScreen;
 import dev.duzo.players.core.FPEntities;
 import dev.duzo.players.core.FPMenus;
@@ -25,6 +26,7 @@ public class PlayersFabricClient implements ClientModInitializer {
 		EntityRendererRegistry.register(FPEntities.FISHING_HOOK.get(), FakeFishingHookRenderer::new);
 		LegacyRodCast.register();
 		MenuScreens.register(FPMenus.FAKE_PLAYER.get(), FakePlayerInventoryScreen::new);
+		MenuScreens.register(FPMenus.CRAFTER_LEARN.get(), FakeCrafterScreen::new);
 		ClientTickEvents.END_CLIENT_TICK.register(PlayersCommonClient::tick);
 		ClientLifecycleEvents.CLIENT_STOPPING.register(client -> PlayersCommonClient.onClientStopping());
 		WorldRenderEvents.LAST.register(ctx -> {
