96c0ff804f0b9ebd03c7528a8d5ffa9c7cc86798
James Hall <james.hall01@outlook.com>
2026-09-05 14:23:05 +0100

    fix(client): swallow edit box keypresses instead of returning editbox result
    (cherry picked from commit 475258ac1cb9fbd704038b660c525d5a38b66df7)

--- common/src/main/java/dev/duzo/players/client/screen/AISubMenuScreen.java
+++ common/src/main/java/dev/duzo/players/client/screen/AISubMenuScreen.java
@@ -473,7 +473,8 @@ public class AISubMenuScreen extends Screen {
 			if (keyCode == InputConstants.KEY_ESCAPE || keyCode == InputConstants.KEY_TAB) {
 				return super.keyPressed(keyCode, scanCode, modifiers);
 			}
-			return this.filterEdit.keyPressed(keyCode, scanCode, modifiers);
+			this.filterEdit.keyPressed(keyCode, scanCode, modifiers);
+			return true;
 		}
 		return super.keyPressed(keyCode, scanCode, modifiers);
 	}
