d6155125da2e52b1dd6e6aedfa93113401f3c63f
James Hall <james.hall01@outlook.com>
2026-09-11 12:45:31 +0100

    feat(requests): show outstanding requests and let the owner cancel them

--- common/src/main/java/dev/duzo/players/network/PlayersNetwork.java
+++ common/src/main/java/dev/duzo/players/network/PlayersNetwork.java
@@ -12,6 +12,7 @@ import dev.duzo.players.network.c2s.OpenCrafterLearnPacketC2S;
 import dev.duzo.players.network.c2s.OpenFakeMenuPacketC2S;
 import dev.duzo.players.network.c2s.RequestSkinDataPacketC2S;
 import dev.duzo.players.network.c2s.RequestItemPacketC2S;
+import dev.duzo.players.network.c2s.CancelRequestPacketC2S;
 import dev.duzo.players.network.c2s.RequestStockPacketC2S;
 import dev.duzo.players.network.s2c.StockListPacketS2C;
 import dev.duzo.players.network.c2s.SetAIFilterPacketC2S;
@@ -40,6 +41,7 @@ public class PlayersNetwork {
 		Network.registerPacket(RequestItemPacketC2S.LOCATION, RequestItemPacketC2S.class, RequestItemPacketC2S::encode, RequestItemPacketC2S::decode, RequestItemPacketC2S::handle);
 		Network.registerPacket(RequestStockPacketC2S.LOCATION, RequestStockPacketC2S.class, RequestStockPacketC2S::encode, RequestStockPacketC2S::decode, RequestStockPacketC2S::handle);
 		Network.registerPacket(StockListPacketS2C.LOCATION, StockListPacketS2C.class, StockListPacketS2C::encode, StockListPacketS2C::decode, StockListPacketS2C::handle);
+		Network.registerPacket(CancelRequestPacketC2S.LOCATION, CancelRequestPacketC2S.class, CancelRequestPacketC2S::encode, CancelRequestPacketC2S::decode, CancelRequestPacketC2S::handle);
 		Network.registerPacket(SetJobPacketC2S.LOCATION, SetJobPacketC2S.class, SetJobPacketC2S::encode, SetJobPacketC2S::decode, SetJobPacketC2S::handle);
 		Network.registerPacket(StartStopJobPacketC2S.LOCATION, StartStopJobPacketC2S.class, StartStopJobPacketC2S::encode, StartStopJobPacketC2S::decode, StartStopJobPacketC2S::handle);
 		Network.registerPacket(GiveAIMarkerPacketC2S.LOCATION, GiveAIMarkerPacketC2S.class, GiveAIMarkerPacketC2S::encode, GiveAIMarkerPacketC2S::decode, GiveAIMarkerPacketC2S::handle);
